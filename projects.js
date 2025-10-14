92ed2210aad3ceee3eb7824c8cb57ddd67cc9851
Wesal Ismail <wsalasmayl363@gmail.com>
2025-10-14 19:38:28 +0300

    copied from old project

---
+++ projects.js
@@ -0,0 +1,73 @@
+export const projects = [{
+    name: 'Movie.w Explorer',
+    about: 'Movie W Explorer is a movie browsing web app that lets users explore, search, and view detailed information about movies',
+    githubLink: 'https://github.com/we2004/movie-explorer.git',
+    websiteLink: 'https://movie-w-explorer.netlify.app/',
+    image: 'images/movie.png'
+}, {
+    name: 'Nebula',
+    about: "Nebula is a front-end web project that connects to NASA's Astronomy Picture of the Day (APOD) API to display space images",
+    githubLink: 'https://github.com/we2004/Nebula.git',
+    websiteLink: 'https://nebula-wesal.netlify.app/',
+    image: 'images/space.png'
+}, {
+    name: 'Weather Desktop App',
+    about: 'Allows users to search for the weather in any city, view current conditions and a 7-day forecast, and toggle between Celsius and Fahrenheit.',
+    githubLink: 'https://github.com/we2004/weather-dashboard-desktop-app.git',
+    websiteLink: 'https://weather-dashboard-app-wesal.netlify.app/',
+    image: 'images/weather.png'
+}, {
+    name: 'Library Management System',
+    about: 'The Library Management System is a desktop application designed to streamline the management of library members and books.',
+    githubLink: 'https://github.com/we2004/Library-Management-System.git',
+    websiteLink: 'https://the-library-management-system.netlify.app/',
+    image: 'images/lib-system.png'
+}, {
+    name: 'Password Generator',
+    about: 'A simple and interactive password generator validates inputs and generates passwords based on user-provided criteria.',
+    githubLink: 'https://github.com/we2004/Password-Generator.git',
+    websiteLink: 'https://password-generator-id.netlify.app/',
+    image: 'images/passwordGenerator.png'
+}, {
+    name: 'Guess the Secret Number',
+    about: 'A simple web-based game where players try to guess a randomly generated number between 1 and 20 within a limited number of attempts.',
+    githubLink: 'https://github.com/we2004/guess-secret-number.git',
+    websiteLink: 'https://guessing-the-secret-number.netlify.app/',
+    image: 'images/secretNumber.png'
+}, {
+    name: 'Coin Flip',
+    about: 'A simple web-based coin flip game where the user plays against the computer.',
+    githubLink: 'https://github.com/we2004/coin-flip.git',
+    websiteLink: 'https://we2004.github.io/coin-flip/',
+    image: 'images/coinweb.png'
+}, {
+    name: 'Multiplication App',
+    about: 'This project is designed to help users practice multiplication.',
+    githubLink: 'https://github.com/we2004/Multiplication-App.git',
+    websiteLink: 'https://we2004.github.io/Multiplication-App/',
+    image: 'images/multiplicationweb.png'
+}, {
+    name: 'Projects Portfolio',
+    about: 'A portfolio that shows a collection of web development projects that I have built.',
+    githubLink: 'https://github.com/we2004/web-projects-portfolio.git',
+    websiteLink: 'https://wesal-projects-page.netlify.app/',
+    image: 'images/projectsweb.png'
+}, {
+    name: 'Anime Landing Page',
+    about: 'used bootstrap , css and html to create a simple landing page for an anime and manga website.',
+    githubLink: 'https://github.com/we2004/AnimeManga-lnadingPage.git',
+    websiteLink: 'https://we2004.github.io/AnimeManga-lnadingPage/',
+    image: 'images/animeweb.png'
+}, {
+    name: 'Sign Up Page',
+    about: 'responsive sign up page using HTML and CSS.',
+    githubLink: 'https://github.com/we2004/sign-up-page.git',
+    websiteLink: 'https://we2004.github.io/sign-up-page/',
+    image: 'images/signupweb.png'
+}, {
+    name: 'Rock Paper Scissors',
+    about: 'A simple and interactive Rock, Paper, Scissors game built with HTML, CSS, and JavaScript. The game allows users to play against the computer, with their scores being tracked and stored using localStorage.',
+    githubLink: 'https://github.com/we2004/rock-paper-scissor.git',
+    websiteLink: 'https://we2004.github.io/rock-paper-scissor/',
+    image: 'images/gameweb.png'
+}]
