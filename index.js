99585084b2dee2e4b1e72b40076e16aebb275089
Wesal Ismail <wsalasmayl363@gmail.com>
2025-10-14 20:03:48 +0300

    removed target attr from website Link

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ projects.forEach((project) => {
                 <h5 class="card-title roboto-slab-font">${project.name}</h5>
                 <p class="card-text roboto-font">${project.about}</p>
                 <a href="${project.githubLink}" target="_blank" class="roboto-font code-link">code</a>
-                <a href="${project.websiteLink}" target="_blank" class="roboto-font website-link">website</a>
+                <a href="${project.websiteLink}" class="roboto-font website-link">website</a>
             </div>
         </div>
     `
